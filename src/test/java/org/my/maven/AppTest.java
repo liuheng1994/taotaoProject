ec0fbc489525a62dbd897345b1f077e0dc7af277
liuliu <792042913@qq.com>
2018-07-09 15:07:13 +0800

    idea整合git

---
+++ src/test/java/org/my/maven/AppTest.java
@@ -0,0 +1,38 @@
+package org.my.maven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit ConnectionTest for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the ConnectionTest case
+     *
+     * @param testName name of the ConnectionTest case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
