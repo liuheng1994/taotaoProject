af74924b54a22e2adca3d7709da3bc83f0d8ade7
liuheng1994 <792042913@qq.com>
2018-07-10 09:42:58 +0800

    liu5的上传

--- src/main/java/org/my/maven/App.java
+++ src/main/java/org/my/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "liu2" );
+        System.out.println( "liu5" );
     }
     public void test(){
         
