9d410d7a00df55170b8aa5a188b886348387504e
liuheng1994 <792042913@qq.com>
2018-07-19 13:32:01 +0800

    Merge branch 'master' into liu7
    # Conflicts:
    #	src/main/java/org/my/maven/App.java

--- src/main/java/org/my/maven/Hello.java
+++ src/main/java/org/my/maven/Hello.java
@@ -4,5 +4,7 @@ package org.my.maven;
  * Created by liuliu on 2018/7/19.
  */
 public class Hello {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        System.out.print("q");
+    }
 }
