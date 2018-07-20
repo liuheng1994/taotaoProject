1e224beb2f83f4177160a77264c728876714e56f
liuheng1994 <792042913@qq.com>
2018-07-20 17:57:18 +0800

    master1

--- src/main/java/org/my/maven/App.java
+++ src/main/java/org/my/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "liu7" );
+        System.out.println( "master" );
     }
     public void test(){
 
