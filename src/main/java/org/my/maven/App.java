36073dcaad3a29ac08ac6ac5e95fc79ad7ab0fb2
liuheng1994 <792042913@qq.com>
2018-07-22 20:56:25 +0800

    Merge remote-tracking branch 'remotes/origin/liu6'
    # Conflicts:
    #	.idea/compiler.xml
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	.idea/vcs.xml
    #	src/main/java/org/my/maven/App.java

--- src/main/java/org/my/maven/App.java
+++ src/main/java/org/my/maven/App.java
@@ -16,20 +16,4 @@ public class App
         System.out.print("master1");
         System.out.println( "liu6" );
     }
-    public void test(){
-
-    }
-    public static void test1(){
-
-    }
-    public static void liu7(){
-
-
-    }
-    public static int main(int a,int b){
-        return a>b?a:b;
-    }
-    public static int copy(){
-        return 4;
-    }
 }
